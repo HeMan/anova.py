45cbfd1bc002453279cb7ccfe0204a09aa3047a5
Jimmy Hedman <jimmy.hedman@gmail.com>
2021-02-14 21:13:00 +0100

    Converted all code to async.
    - Had to change API a bit since properties and specificaly setters
      doesn't work with async.

--- anovapy/__init__.py
+++ anovapy/__init__.py
@@ -4,11 +4,13 @@
 Module to interface with the the Anova Precision Cooker API.
 """
 
+import asyncio
 import json
 import time
 from typing import Union
 
-import requests
+import aiohttp
+from aiohttp import ClientSession
 
 
 class AnovaCooker:
@@ -21,41 +23,38 @@ class AnovaCooker:
         """
         TODO: stop spoofing user agent
         """
-        base_url = "https://api.anovaculinary.com/cookers/"
-        self._url = base_url + cooker_id + "?secret=" + secret
-        self._jobs_url = base_url + cooker_id + "/jobs?secret=" + secret
-        self._headers = {"User-Agent": "Cook/1.2.2 (iPhone; iOS 10.2; Scale/2.00)"}
+        base_url = f"https://api.anovaculinary.com/cookers/{cooker_id}"
+        self._url = f"{base_url}?secret={secret}"
+        self._jobs_url = f"{base_url}/jobs?secret={secret}"
+        self._headers = {
+            "User-Agent": "Cook/1.2.2 (iPhone; iOS 10.2; Scale/2.00)",
+        }
 
-    @property
-    def current_temperature(self) -> Union[int, float]:
-        return self.get_status_object()["current_temp"]
+    async def get_current_temperature(self) -> Union[int, float]:
+        return (await self.get_status_object())["current_temp"]
 
-    @property
-    def target_temperature(self) -> Union[int, float]:
-        return self.get_status_object()["target_temp"]
+    async def get_target_temperature(self) -> Union[int, float]:
+        return (await self.get_status_object())["target_temp"]
 
-    @target_temperature.setter
-    def target_temperature(self, temperature: Union[int, float]) -> str:
+    async def set_target_temperature(self, temperature: Union[int, float]) -> str:
         payload = {"target_temp": temperature}
-        return self._post_request(self._url, payload)
+        return await self._post_request(self._url, payload)
 
-    @property
-    def temperature_unit(self) -> str:
+    async def get_temperature_unit(self) -> str:
         """
         valid values are:
         'c' for celsius,
         'f' for Fahrenheit and
         '' for the currently set default unit.
         """
-        return self.get_status_object()["temp_unit"]
+        return (await self.get_status_object())["temp_unit"]
 
-    @temperature_unit.setter
-    def temperature_unit(self, unit: str):
+    async def set_temperature_unit(self, unit: str):
         if unit not in ("c", "f", ""):
             raise ValueError("temperature_unit has to be either 'c', 'f' or ''")
 
         payload = {"temp_unit": unit}
-        self._post_request(self._url, payload)
+        await self._post_request(self._url, payload)
 
     @property
     def running(self) -> bool:
@@ -83,66 +82,77 @@ class AnovaCooker:
         payload = {"speaker_mode": state}
         self._post_request(self._url, payload)
 
-    def _get_request(self, url: str) -> str:
-        request = requests.get(url, headers=self._headers)
-        return json.dumps(request.json(), indent=4, sort_keys=True)
+    async def _get_request(self, url: str) -> str:
+        async with ClientSession() as session:
+            request = await session.get(url, headers=self._headers)
+            request.raise_for_status()
+            return json.dumps(
+                await request.json(content_type="text/plain"), indent=4, sort_keys=True
+            )
 
-    def _post_request(self, url: str, payload: dict) -> str:
-        request = requests.post(url, json=payload, headers=self._headers)
-        return json.dumps(request.json(), indent=4, sort_keys=True)
+    async def _post_request(self, url: str, payload: dict) -> str:
+        async with ClientSession() as session:
+            request = await session.post(
+                url,
+                json=payload,
+                headers=self._headers,
+            )
+            request.raise_for_status()
+            return json.dumps(
+                await request.json(content_type="text/plain"), indent=4, sort_keys=True
+            )
 
-    def _set_timer(self, seconds: Union[int, float]) -> str:
+    async def _set_timer(self, seconds: Union[int, float]) -> str:
         """
         calling this on its own does nothing. use create_job() instead.
         """
         payload = {"timer_length": seconds}
-        return self._post_request(self._jobs_url, payload)
+        return await self._post_request(self._jobs_url, payload)
 
-    def stop_alarm(self) -> str:
+    async def stop_alarm(self) -> str:
         """
         disables the alarm that signals the end of a cook.
         """
         payload = {"alarm_active": False}
-        return self._post_request(self._url, payload)
+        return await self._post_request(self._url, payload)
 
-    def get_status(self) -> str:
+    async def get_status(self) -> str:
         """
         returns current state as json.
         """
-        return self._get_request(self._url)
+        return await self._get_request(self._url)
 
-    def get_status_object(self) -> dict:
+    async def get_status_object(self) -> dict:
         """
         returns current status as a dictionary.
         """
-        return json.loads(self.get_status())["status"]
+        return json.loads(await self.get_status())["status"]
 
-    def get_jobs(self) -> str:
+    async def get_jobs(self) -> str:
         """
         returns all jobs (including past ones) as json.
         """
-        return self._get_request(self._jobs_url)
+        return await self._get_request(self._jobs_url)
 
-    def get_jobs_object(self) -> dict:
+    async def get_jobs_object(self) -> dict:
         """
         returns all jobs (including past ones) as a dictionary.
         """
-        return json.loads(self.get_jobs())
+        return json.loads(await self.get_jobs())
 
-    def get_current_job(self) -> str:
+    async def get_current_job(self) -> str:
         """
         returns the current job as a string.
         """
-        status = self.get_status()
-        status = json.loads(status)
+        status = await self.get_status_object()
         # Getting the current job will fail if ones doesn't exist
-        if "current_job" in status["status"]:
-            job_id = status["status"]["current_job"]["job_id"]
+        if "current_job" in status:
+            job_id = status["current_job"]["job_id"]
         else:
             job_id = ""
         return job_id
 
-    def create_job(
+    async def create_job(
         self,
         temperature: Union[int, float],
         seconds: Union[int, float],
@@ -154,10 +164,10 @@ class AnovaCooker:
         """
 
         if not temperature_unit:
-            temperature_unit = self.temperature_unit
+            temperature_unit = await self.get_temperature_unit()
 
-        self.target_temperature = temperature
-        self._set_timer(seconds)
+        await self.set_target_temperature(temperature)
+        # await self._set_timer(seconds)
 
         temp_unit_expansion = {"c": "Celsius", "f": "Fahrenheit"}
         payload = {
@@ -180,15 +190,15 @@ class AnovaCooker:
             "timer_length": seconds,
         }
 
-        return self._post_request(self._jobs_url, payload)
+        return await self._post_request(self._jobs_url, payload)
 
-    def start_ice_bath(self, temperature_unit: str = "") -> str:
+    async def start_ice_bath(self, temperature_unit: str = ""):
         """
         starts the ice bath and monitoring
         """
 
         if not temperature_unit:
-            temperature_unit = self.temperature_unit
+            temperature_unit = await self.get_temperature_unit()
 
         # Set the proper threshold_temp for degrees f or degrees c
         if temperature_unit == "f":
@@ -209,15 +219,14 @@ class AnovaCooker:
             "threshold_temp": threshold_temp,
             "timer_length": 0,
         }
-        post1 = self._post_request(self._jobs_url, payload)
+        await self._post_request(self._jobs_url, payload)
 
         # We need to wait for the test to finish - 15 seconds is probably too long
-        time.sleep(15)
+        await asyncio.sleep(15)
 
         # Confirm that the temperature is at or below the threshold_temp
-        status = self.get_status()
-        status = json.loads(status)
-        current_temp = status["status"]["current_temp"]
+        status = await self.get_status_object()
+        current_temp = status["current_temp"]
         if current_temp <= threshold_temp:
             # We are at or below temp, start the monitoring
             payload = {
@@ -234,22 +243,20 @@ class AnovaCooker:
                 "threshold_temp": threshold_temp,
                 "timer_length": 0,
             }
-            return self._post_request(self._jobs_url, payload)
+            return await self._post_request(self._jobs_url, payload)
         else:
             print("Ice bath not at temperature, aborting!")
 
-    def stop_ice_bath(self) -> str:
+    async def stop_ice_bath(self):
         """
         disables the ice bath monitoring
         """
         # Get the current job ID - this MUST be posted to the job or it will fail
-        job_id = self.get_current_job()
+        job_id = await self.get_current_job()
         # Don't try to stop the job if it doesn't exist
         if job_id != "":
             payload = {"is_running": False, "timer_length": 0}
             # We need to insert the job ID into the URL
-            new_url = self._jobs_url.replace(
-                "/jobs?secret=", "/jobs/" + job_id + "?secret="
-            )
-            self._post_request(new_url, payload)
-            self.stop_alarm()
+            new_url = self._jobs_url.replace("/jobs?secret=", f"/jobs/{job_id}?secret=")
+            await self._post_request(new_url, payload)
+            await self.stop_alarm()
